8fc98392f1f68fecc8a720babb05146b0e584fce
aSh1651 <arsh.hundal30@gmail.com>
2026-04-25 20:10:36 +0200

    1

---
+++ Arsh/inc/sbus.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include "main.h"
+#ifndef INC_SBUS_H_
+#define INC_SBUS_H_
+typedef struct {
+    uint16_t channels[16];
+    uint8_t frame_lost;
+    uint8_t failsafe;
+} SbusFrame;
+
+
+void decode_sbus(uint8_t *buffer, SbusFrame *sbus);
+//void print_channels();
+
+#endif /* INC_SBUS_H_ */
