c5e33fb728350c4713df02f167dea152318500d0
Asmabenamor-coder <asmabenamor2@outlook.fr>
2026-05-01 14:15:44 +0200

    sbus.h

--- asma/sbus.h
+++ asma/sbus.h
@@ -1,1 +1,20 @@
-co
+/*
+ * sbus.h
+ *
+ *  Created on: Apr 13, 2026
+ *      Author: asmab
+ */
+
+#ifndef INC_SBUS_H_
+#define INC_SBUS_H_
+
+#include "main.h"
+#include <stdint.h>
+
+extern uint16_t channels[16];
+
+void decode_sbus(uint8_t *buf);
+uint8_t read_sbus_frame(UART_HandleTypeDef *huart, uint8_t *buf);
+
+#endif /* INC_SBUS_H_ */
+
