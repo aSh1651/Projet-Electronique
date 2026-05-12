cdbf22aa7b867697c4cfab28276bf5782e446571
aSh1651 <arsh.hundal30@gmail.com>
2026-05-12 20:11:35 +0200

    Update roues_PWM.h

--- asma/roues_PWM.h
+++ asma/roues_PWM.h
@@ -2,8 +2,12 @@
 #define SRC_ROUE_PWM_H_
 
 void setup_roue_PWM();
-
 void loop_roue_PWM();
-
+int ch_b[4]= {1084, 957, 956, 1030};
+int ch_min=200;
+int ch_max=2000;
+int pwm_min = 1000;
+int pwm_max = 60000;
+void convertir_sbus_to_pwm();
 
 #endif /* SRC_ROUE_PWM_H_ */
