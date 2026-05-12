cceb416c4791a659b20c7a37d94e657280adb606
aSh1651 <arsh.hundal30@gmail.com>
2026-05-12 21:51:12 +0200

    Update roues_PWM.c

--- asma/roues_PWM.c
+++ asma/roues_PWM.c
@@ -17,10 +17,28 @@ void loop_roue_PWM(){
 			printf("test_boucle2");
 		}
 }
-void convertir_sbus_to_pwm(int i, uint8_t* sbus_buffer ){
-  int pmw= ((sbus_buffer[i] - ch_min) *( pwm_max- pwm_min ))/
-                    (ch_max - ch_min) +
-                   pwm_min;
-  TIM1->CCR1 = pmw;
-  printf("ccr%d=|\r\n ",pmw );
+void convertir_sbus_to_pwm(int i, uint16_t* sbus_channel ){
+	uint16_t neutre = ch_b[i];
+	uint16_t sbus_valeur=sbus_channel[i];
+	uint32_t pwm = 0;
+	// securité pour éviter de sortir de la plage de donnée
+	if (sbus_valeur < ch_min) sbus_valeur = ch_min;
+	if (sbus_valeur > ch_max) sbus_valeur = ch_max;
+	
+	if (sbus_valeur > neutre) {
+	        // Zone Haute : du neutre (0%) vers le max (100%)
+	        // Formule : (Valeur - Depart) * (Arrivée_Max / Plage_SBUS)
+	        pwm = (uint32_t)(sbus_valeur - neutre) * pwm_max / (ch_max - neutre);
+	    } 
+	    else if (sbus_valeur < neutre) {
+	        // Zone Basse : du neutre (0%) vers le min (100%)
+	        pwm = (uint32_t)(neutre - sbus_valeur) * pwm_max / (neutre - ch_min);
+	    } 
+	    else {
+	        pwm = 0; // Pile sur le neutre
+	    }
+	TIM1->CCR1 = pmw;
+	HAL_Delay(5);
+  printf("Canal %d = %lu \r\n", i, pwm);// lu =long unsigned
 }
+
