13a3e8c3927ba47ac7e57e86aa60c09569d93578
aSh1651 <arsh.hundal30@gmail.com>
2026-05-12 20:06:24 +0200

    Update sbus.c

--- asma/sbus.c
+++ asma/sbus.c
@@ -6,47 +6,32 @@
  */
 #include "sbus.h"
 
-//const
-void decode_sbus(uint8_t *buffer, SbusFrame *sbus) {
+void decode_sbus(uint8_t *sbus_buffer, uint16_t *sbus_channels) {
     // Vérification du Header (0x0F) et du Footer (0x00)
-    if (buffer[0] != 0x0F || buffer[24] != 0x00) return;
+
+    if (sbus_buffer[0] != 0x0F) return;
 
     // Décodage des 11 bits par canal via décalage de bits
-    sbus->channels[0]  = ((buffer[1]    | buffer[2] << 8)                          & 0x07FF);
-    sbus->channels[1]  = ((buffer[2] >> 3 | buffer[3] << 5)                          & 0x07FF);
-    sbus->channels[2]  = ((buffer[3] >> 6 | buffer[4] << 2 | buffer[5] << 10)       & 0x07FF);
-    sbus->channels[3]  = ((buffer[5] >> 1 | buffer[6] << 7)                          & 0x07FF);
-    sbus->channels[4]  = ((buffer[6] >> 4 | buffer[7] << 4)                          & 0x07FF);
-    sbus->channels[5]  = ((buffer[7] >> 7 | buffer[8] << 1 | buffer[9] << 9)        & 0x07FF);
-    sbus->channels[6]  = ((buffer[9] >> 2 | buffer[10] << 6)                         & 0x07FF);
-    sbus->channels[7]  = ((buffer[10] >> 5 | buffer[11] << 3)                        & 0x07FF);
-    sbus->channels[8]  = ((buffer[12]   | buffer[13] << 8)                         & 0x07FF);
-    sbus->channels[9]  = ((buffer[13] >> 3 | buffer[14] << 5)                        & 0x07FF);
-    sbus->channels[10] = ((buffer[14] >> 6 | buffer[15] << 2 | buffer[16] << 10)      & 0x07FF);
-    sbus->channels[11] = ((buffer[16] >> 1 | buffer[17] << 7)                        & 0x07FF);
-    sbus->channels[12] = ((buffer[17] >> 4 | buffer[18] << 4)                        & 0x07FF);
-    sbus->channels[13] = ((buffer[18] >> 7 | buffer[19] << 1 | buffer[20] << 9)      & 0x07FF);
-    sbus->channels[14] = ((buffer[20] >> 2 | buffer[21] << 6)                        & 0x07FF);
-    sbus->channels[15] = ((buffer[21] >> 5 | buffer[22] << 3)                        & 0x07FF);
+    sbus_channels[0]  = ((sbus_buffer[1]      | sbus_buffer[2]  << 8) & 0x07FF);
+    sbus_channels[1]  = ((sbus_buffer[2] >> 3 | sbus_buffer[3]  << 5) & 0x07FF);
+    sbus_channels[2]  = ((sbus_buffer[3] >> 6 | sbus_buffer[4]  << 2 | sbus_buffer[5]  << 10) & 0x07FF);
+    sbus_channels[3]  = ((sbus_buffer[5] >> 1 | sbus_buffer[6]  << 7) & 0x07FF);
+    sbus_channels[4]  = ((sbus_buffer[6] >> 4 | sbus_buffer[7]  << 4) & 0x07FF);
+    sbus_channels[5]  = ((sbus_buffer[7] >> 7 | sbus_buffer[8]  << 1 | sbus_buffer[9]  << 9) & 0x07FF);
+    sbus_channels[6]  = ((sbus_buffer[9] >> 2 | sbus_buffer[10] << 6) & 0x07FF);
+    sbus_channels[7]  = ((sbus_buffer[10] >> 5 | sbus_buffer[11] << 3) & 0x07FF);
+    sbus_channels[8]  = ((sbus_buffer[12]     | sbus_buffer[13] << 8) & 0x07FF);
+    sbus_channels[9]  = ((sbus_buffer[13] >> 3 | sbus_buffer[14] << 5) & 0x07FF);
+    sbus_channels[10] = ((sbus_buffer[14] >> 6 | sbus_buffer[15] << 2 | sbus_buffer[16] << 10) & 0x07FF);
+    sbus_channels[11] = ((sbus_buffer[16] >> 1 | sbus_buffer[17] << 7) & 0x07FF);
+    sbus_channels[12] = ((sbus_buffer[17] >> 4 | sbus_buffer[18] << 4) & 0x07FF);
+    sbus_channels[13] = ((sbus_buffer[18] >> 7 | sbus_buffer[19] << 1 | sbus_buffer[20] << 9) & 0x07FF);
+    sbus_channels[14] = ((sbus_buffer[20] >> 2 | sbus_buffer[21] << 6) & 0x07FF);
+    sbus_channels[15] = ((sbus_buffer[21] >> 5 | sbus_buffer[22] << 3) & 0x07FF);
+//    // Byte 23 contient les drapeaux (Failsafe et Frame Lost)
+//    sbus->frame_lost = (buffer[23] & 0x04);
+//    sbus->failsafe   = (buffer[23] & 0x08);
 
-    // Byte 23 contient les drapeaux (Failsafe et Frame Lost)
-    sbus->frame_lost = (buffer[23] & 0x04);
-    sbus->failsafe   = (buffer[23] & 0x08);
 }
-
-int ch_b= [1084, 957, 956, 1030];
-ch_min=200;
-ch_max=2000;
-
-int pwm_min = 1000;
-int pwm_max = 60000;
-
-
-int convertir_sbus_to_pwm(int i, ){
-  return ((sbus_value[i] - ch_min) *( pwm_max- pwm_min ))/  
-                    (ch_max - ch_min) + 
-                   pwm_min;
-}
-  
 
 
