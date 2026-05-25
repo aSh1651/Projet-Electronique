c2e6d10b850eb9fcd15f5522dbb74c814152a59c
Asmabenamor-coder <asmabenamor2@outlook.fr>
2026-05-25 13:43:25 +0200

    Update roue.h

--- Code_final/Inc/roue.h
+++ Code_final/Inc/roue.h
@@ -1,1 +1,12 @@
+#ifndef INC_ROUES_H_
+#define INC_ROUES_H_
+
+void setup_roue_PWM(void);
+void roue_stop(void);
+void roue_marche(int direction, int roue_droite);
+
+int decode_inclinaison(int channel, int repos);
+void controle_roues_sbus(void);
+
+#endif
 
