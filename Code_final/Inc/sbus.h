41223563178e8febc12e96efe37d3b62b1a5d84a
Asmabenamor-coder <asmabenamor2@outlook.fr>
2026-05-25 13:43:57 +0200

    Add SBUS header file with definitions and functions

--- Code_final/Inc/sbus.h
+++ Code_final/Inc/sbus.h
@@ -1,1 +1,23 @@
+#ifndef INC_SBUS_H_
+#define INC_SBUS_H_
+
+#include <stdint.h>
+#include "main.h"
+
+extern UART_HandleTypeDef huart3;
+
+extern uint8_t sbus_buffer[25];
+extern uint16_t sbus_channels[16];
+
+typedef struct
+{
+	uint16_t channels[16];
+	uint8_t frame_lost;
+	uint8_t failsafe;
+} SbusFrame;
+
+void loop_sbus(void);
+void decode_sbus(uint8_t *buffer, SbusFrame *sbus);
+
+#endif
 
